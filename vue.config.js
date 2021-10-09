608c9f2f62763b6d9d2a026433a46f295edd7d48
Jasur Khakimov <jasur.khakimov9@gmail.com>
2021-10-09 22:46:56 +0500

    title fix

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,11 @@
 module.exports = {
   transpileDependencies: [
     'vuetify'
-  ]
+  ],
+  chainWebpack: config => {
+    config.plugin('html').tap(args => {
+        args[0].title = 'Divavictoria Atelier';
+        return args;
+    });
+}
 }
